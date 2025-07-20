cd1592e4dfa8811b9d59c8a6aa76b66220fa6d6c
izzalDev <rizal.fadlullah@gmail.com>
2025-07-20 12:40:41 +0700

    Initial commit

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,11 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>", {})
+  end
+}
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,8 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local builtin = require("telescope.builtin")
+    vim.keymap.set("n", "<C-p>", function() builtin.find_files({initial_mode = "normal"}) end, {})
+  end
+}
