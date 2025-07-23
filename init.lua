0d62d6e0f21781fb476a4534e298b04f2b9a4c57
izzalDev <izzalDev>
2025-07-23 15:00:24 +0100

    error

--- init.lua
+++ init.lua
@@ -1,3 +1,15 @@
-require("options")
-require("plugins")
+-- File: C:\Users\Rizal Fadlullah\appdata\local\nvim\init.lua
 
+vim.opt.rtp.prepend("lazy.nvim")
+
+require("options")
+
+-- Pastikan lazy sudah dimuat sebelum setup dipanggil
+local lazy = require("lazy")
+
+lazy.setup({
+  spec = {
+    { import = "plugins" }, -- Ini akan mengimpor semua file dari direktori lua/plugins
+  },
+  -- Tambahkan opsi lazy.nvim lainnya di sini jika ada (misalnya: defaults, checker, change_detection)
+})
