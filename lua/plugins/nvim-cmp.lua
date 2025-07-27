6381c54f16eca7cbf6dd3f44a389798977d363fd
izzalDev <rizal.fadlullah@gmail.com>
2025-07-27 15:07:18 +0700

    feat: Setup nvim-cmp for Neovim autocompletion Mengintegrasikan nvim-cmp dengan sumber (LSP, buffer, path, nvim_lua) dan keybinding esensial untuk penyelesaian dan navigasi.

---
+++ lua/plugins/nvim-cmp.lua
@@ -0,0 +1,80 @@
+return {
+		"hrsh7th/nvim-cmp",
+		version = false, -- last release is way too old
+		event = "InsertEnter",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-nvim-lua",
+		},
+		opts = function()
+			local cmp = require("cmp")
+			local check_backspace = function()
+				local col = vim.fn.col(".") - 1
+				return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
+			end
+
+			return {
+				completion = {
+					completeopt = "menu,menuone,noinsert",
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }), 
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif check_backspace() then
+							fallback()
+						else
+							fallback()
+						end
+					end, {
+						"i",
+						"s",
+					}),
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						else
+							fallback()
+						end
+					end, {
+						"i",
+						"s",
+					}),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "buffer" },
+					{ name = "path" },
+					{ name = "nvim_lua" },
+				}),
+				formatting = {
+					fields = { "kind", "abbr", "menu" },
+					format = function(entry, vim_item)
+						vim_item.menu = vim.api.nvim_get_mode().mode == "c" and "" or vim_item.kind
+						-- vim_item.menu = ({
+						-- 	nvim_lsp = "(LSP)",
+						-- 	buffer = "(Buffer)",
+						-- 	path = "(Path)",
+						-- 	codeium = "(Codeium)",
+						-- })[entry.source.name]
+						return vim_item
+					end,
+				},
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+				experimental = {
+					ghost_text = false,
+					native_menu = false,
+				},
+			}
+		end,
+	}
