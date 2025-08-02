bc2b17fa88e9a90123906a40711d215d108d7f5d
izzalDev <rizal.fadlullah@gmail.com>
2025-08-02 21:17:19 +0700

    feat: Change default integrated terminal shell to PowerShell Core (pwsh)

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,6 +1,8 @@
 return {
 	"akinsho/toggleterm.nvim",
 	config = function()
-		require("toggleterm").setup({})
+		require("toggleterm").setup({
+			shell = vim.fn.has("win32") == 1 and "pwsh -nologo" or nil,
+		})
 	end,
 }
