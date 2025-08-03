85f42d3fc4013f2fb7cbce7982cce529a75bfaf3
izzalDev <rizal.fadlullah@gmail.com>
2025-08-03 13:45:10 +0700

    fix: Fixed deprecated vim.fn.sign_define()

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -2,17 +2,20 @@ return {
 	"neovim/nvim-lspconfig",
 	config = function()
 		vim.diagnostic.config({
+			signs = {
+				text = {
+					[vim.diagnostic.severity.ERROR] = "",
+					[vim.diagnostic.severity.WARN] = "",
+					[vim.diagnostic.severity.INFO] = "",
+					[vim.diagnostic.severity.HINT] = "",
+				},
+			},
 			virtual_text = true,
+			severity_sort = true,
 		})
 
 		require("lspconfig").lua_ls.setup({})
 		vim.keymap.set("n", "K", vim.lsp.buf.hover)
 		vim.keymap.set("n", "gd", vim.lsp.buf.definition)
-
-		local sign = { Error = "", Warn = "", Hint = "", Info = "" }
-		for type, icon in pairs(sign) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-		end
 	end,
 }
