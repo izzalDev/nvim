b943cd1c1cda943d9b013277e75227e94d8d2b5d
izzalDev <rizal.fadlullah@gmail.com>
2025-08-03 11:53:27 +0700

    feat: Show lsp diagnostic virtual text on buffer

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,6 +1,10 @@
 return {
 	"neovim/nvim-lspconfig",
 	config = function()
+		vim.diagnostic.config({
+			virtual_text = true,
+		})
+
 		require("lspconfig").lua_ls.setup({})
 		vim.keymap.set("n", "K", vim.lsp.buf.hover)
 		vim.keymap.set("n", "gd", vim.lsp.buf.definition)
