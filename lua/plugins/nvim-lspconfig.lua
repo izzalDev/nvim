dbc6aee8a894663a1009c0ff64aafaec39fd2422
izzalDev <rizal.fadlullah@gmail.com>
2025-08-03 13:18:26 +0700

    feat: Changes default lsp diagnostic sign using nerd symbol for better experience

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -8,5 +8,11 @@ return {
 		require("lspconfig").lua_ls.setup({})
 		vim.keymap.set("n", "K", vim.lsp.buf.hover)
 		vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+
+		local sign = { Error = "", Warn = "", Hint = "", Info = "" }
+		for type, icon in pairs(sign) do
+			local hl = "DiagnosticSign" .. type
+			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+		end
 	end,
 }
