3aa3a8d21bd804a2d3eef617de296a633d3967f9
izzalDev <rizal.fadlullah@gmail.com>
2025-08-09 18:28:22 +0700

    autoclose neo-tree on buffer enter

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,13 +1,19 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    require("neo-tree").setup({
-      close_if_last_window = true,
-    })
-  end,
+	"nvim-neo-tree/neo-tree.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			close_if_last_window = true,
+		})
+
+		vim.api.nvim_create_autocmd({ "BufEnter" }, {
+			callback = function()
+				vim.cmd("Neotree close")
+			end,
+		})
+	end,
 }
