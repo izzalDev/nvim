2ad36788c622c5e3db8f7fb44d8c271c32019214
izzalDev <rizal.fadlullah@gmail.com>
2025-08-14 20:02:18 +0700

    feat(noice): Changed cmdline positon to center

--- lua/configs/noice.lua
+++ lua/configs/noice.lua
@@ -23,10 +23,7 @@ return function()
 			},
 		},
 		presets = {
-			bottom_search = true,
-			command_palette = true,
 			long_message_to_split = true,
-			inc_rename = false,
 			lsp_doc_border = true, -- Enable noice border handling
 		},
 		-- Custom views dengan rounded border
