dca75758e26d8b3fa6343cc6985aeed624b04487
izzalDev <rizal.fadlullah@gmail.com>
2026-06-25 13:54:21 +0700

    feat(vimtex): specify which pdf viewer based on platform

--- lua/plugins/tools/vimtex.lua
+++ lua/plugins/tools/vimtex.lua
@@ -15,7 +15,13 @@ return {
 		{ "<leader>la", "<cmd>VimtexContext<cr>", desc = "LaTeX Show Context under Cursor" },
 	},
 	init = function()
-		vim.g.vimtex_view_method = "mupdf"
+		if vim.fn.has("mac") == 1 then
+			vim.g.vimtex_view_method = "skim"
+		elseif vim.fn.has("win32") == 1 then
+			vim.g.vimtex_view_method = "sumpdf"
+		elseif vim.fn.has("unix") == 1 then
+			vim.g.vimtex_view_method = "zathura"
+		end
 		vim.g.vimtex_mappings_enabled = 0
 		vim.g.vimtex_quickfix_open_on_warning = 0
 		vim.g.vimtex_toc_config = {
